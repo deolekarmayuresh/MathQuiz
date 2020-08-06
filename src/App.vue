e7c6f08ecf6eddb22efaf07c930eb54a4f1f292e
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-06 14:05:34 +0530

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <!-- adding transition -->
                 <transition name="flip" mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
                 </transition>
@@ -45,7 +46,7 @@
 
 <style>
     .flip-enter {
-        /*transform: rotateY(0deg);*/
+        /* transform: rotateY(0deg); */
     }
 
     .flip-enter-active {
