a3d5383f5b843be20c22e43f82c4d37c93e208aa
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-14 15:15:22 +0530

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 class="text-center">Math Quiz</h1>
+                <h1 class="text-center" style="color:red">Math Quiz</h1>
             </div>
         </div>
         <hr>
